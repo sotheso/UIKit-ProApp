0689c1357e891d4d394f967f4450b3ff70a0b29b
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-18 17:24:27 +0330

    Connecting Storyboards to Code -> کاستوم کردن ویو ها با کلاس های کاستوم شده  > سینک شده با بخش تنظیمات main

--- UIKit-ProApp/ViewController.swift
+++ UIKit-ProApp/ViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:
+                            
+    UIViewController {
+    
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var blurView: UIVisualEffectView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+/// به جای استفاده از این کد ها در هر فیچر برنامه از کلاس های کمک کننده فایل Helper استفاده میکنیم
+//        cardView.layer.cornerRadius = 30
+//        cardView.layer.cornerCurve = .continuous
+//        blurView.layer.cornerRadius = 30
+//        blurView.layer.cornerCurve = .continuous
+//        
+//        blurView.layer.masksToBounds = true
+//        
+//        cardView.layer.shadowColor = UIColor(named: "Shadow")!.cgColor
+//        cardView.layer.shadowOpacity = 0.5
+//        cardView.layer.shadowRadius = 30
+//        cardView.layer.shadowOffset = CGSize(width: 0, height: 10 )
+/// اینطوری میتونی با import کردن کلاس مربوطه
+/// در بخش main تنظیمات
+/// ویوی خودت رو کاستوم کنی
+        
     }
 
 
