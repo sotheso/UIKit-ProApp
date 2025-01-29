bf77a969f882b6d1ff8e082f2cbc3354bfada260
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-29 11:57:08 +0330

    Implementing Firebase Authentication: وصل کردن ساخت اکانت به firebase

--- UIKit-ProApp/View Controllers/LoginViewController.swift
+++ UIKit-ProApp/View Controllers/LoginViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+// واسه اینکه ببینیم فیلد ها خالی نباشن
+import Combine
 
 class LoginViewController: UIViewController {
 
@@ -15,6 +18,11 @@ class LoginViewController: UIViewController {
     @IBOutlet var accessoryBtn: UIButton!
     @IBOutlet var passwordTextfield: UITextField!
     @IBOutlet var emailTextfield: UITextField!
+    
+    // واسه اینکه ببینیم فیلد ها خالی نباشن
+    var emailKHALI = true
+    var passwordKHALI = true
+    private var tokens: Set<AnyCancellable> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +34,58 @@ class LoginViewController: UIViewController {
             self.loginCard.alpha = 1
             self.loginCard.frame = self.loginCard.frame.offsetBy(dx: 0, dy: -400)
         }
+        
+        
+        // گوش دادن به تغییرات درون فیلد
+        emailTextfield.publisher(for: \.text)
+            .sink { newValue in
+                self.emailKHALI = (newValue == "" || newValue == nil)
+            }
+            .store(in: &tokens)
+        
+        passwordTextfield.publisher(for: \.text)
+            .sink { newValue in
+                self.passwordKHALI = (newValue == "" || newValue == nil)
+            }
+            .store(in: &tokens)
+        
     }
+    
+    
 
+    // اتصال دکمه های ساخت و singin به Firebase
+    @IBAction func primaryBtnAction(_ sender: Any) {
+        // اگر دوتا فیلد ایمیل و پسورد خالی باشه به کاربر هشدار بده
+        if (emailKHALI || passwordKHALI) {
+            let alert = UIAlertController(title: "Missing Information", message: "Please make sure to enter a valid email address and a paswword", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            // حال اگه پر بود
+            Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+                authResult, error in
+                guard error == nil else {
+                    print(error!.localizedDescription)
+                    return
+                }
+                
+                // اگر کاربر با موفقیت اکانت ساخت:
+                // به صفحه home وصل شه
+                self.goToHomeScreen()
+            }
+            
+        }
+    }
+    
+    @IBAction func accessoryBtnAction(_ sender: Any) {
+    }
+    
+    // تابع رفتن به صفحه اصلی
+    func goToHomeScreen() {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CustomTabBarViewController") as! CustomTabBarViewController
+        
+        vc.modalTransitionStyle = .flipHorizontal
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+    }
 }
